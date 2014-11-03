9d9c7161f3b44810d3e4f7a8b0e3723680a3dfa1
Earth Ponce Maniebo <earth@codesignate.com>
2014-11-03 23:10:06 +0800

    Change implementation of data storage from dict of a dicto to list of dictionary.

--- app.py
+++ app.py
@@ -1,18 +1,49 @@
 import os
+import pdfkit
 import requests
 import xml.etree.ElementTree as ET
 
+# URL of RSS/Atom Feed
 url = 'http://www.inquirer.net/fullfeed'
+
+# Send get request 
 resp = requests.get(url)
+
+# Parse XML
+# We can either save it in a file before reading or
+# Read it directly from the response
 
 # xml_doc = ET.fromstring(resp.content)
 xml_doc = ET.parse('test.xml')
 
-
-
+# Nested dictionary to store news
+news = []
+indentifier_str =  'headline'
+counter = 0
 
 for entity in xml_doc.iter('item'):
-    print entity.find("title").text
-    print entity.find("description").text
-    print entity.find("link").text
-    print entity.find("pubDate").text
+    
+    # Generate the unique name of the index
+    counter = counter + 1
+    index = indentifier_str + str(counter)
+
+    # Dictionary to store inside the news dictionary
+    title       = entity.find("title").text
+    link        = entity.find("link").text
+    pubDate     = entity.find("pubDate").text
+    description = entity.find("description").text
+
+    headline = {
+        'title' : title,
+        'link' : link,
+        'pubDate' : pubDate,
+        'description' : description
+    }
+
+    news.append(headline)
+
+for d in news:
+
+
+
+# pdfkit.from_url('http://www.inquirer.net/fullfeed', 'out.pdf')
